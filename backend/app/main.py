fc4a85548bf2b2450e03881ba578159408db0767
Saathwika <saathwikamachaa@gmail.com>
2026-04-22 20:59:20 +0530

    Add files via upload

--- backend/app/main.py
+++ backend/app/main.py
@@ -85,7 +85,11 @@ def ui():
     """
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://conductivity-front.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
